293a92547cbba9a23dfa5785d4cc627c40d27d09
NottSr <leonardo.br4@hotmail.com>
2022-09-06 12:36:52 -0500

    Task 4 fixed

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,7 +7,7 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 DROP USER IF EXISTS 'hbnb_test'@'localhost';
 CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
-GRANT ALL PRIVILEGES ON `hbnb_test`.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 
 FLUSH PRIVILEGES;
