a4bf36d4b5293a861f3cac1325880e511f6aff42
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-09-06 12:32:40 -0500

    setup 4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,13 @@
+-- Script that prepares a MySQL server for the project
+-- Set ALL privileges on the database hbnb_dev_db
+-- Set SELECT privilege on the database performance_schema
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+DROP USER IF EXISTS 'hbnb_test'@'localhost';
+CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+GRANT ALL PRIVILEGES ON `hbnb_test`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
+
+FLUSH PRIVILEGES;
