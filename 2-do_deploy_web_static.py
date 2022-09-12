5fed2cb61bb6e6aaa497f57ed970a0285acda5aa
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-09-11 22:18:16 -0500

    deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,36 +1,33 @@
 #!/usr/bin/python3
-"""
-Fabric script that distributes an archive to your web
-servers, using the function do_deploy
-"""
+""" Write a Fabric script that generates a .tgz archive from the contents of
+the web_static folder of your AirBnB Clone repo, using the function do_pack."""
 
-from fabric.api import put, run, env
-from os.path import exists
+from fabric.api import *
+from datetime import datetime
+from os import path
+
+
 env.hosts = ['3.90.180.30', '18.206.165.178']
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if not exists(archive_path):
+    """ Deploy the file in specific folders in the servers """
+    if path.isfile(archive_path) is False:
         return False
+    filetgz = archive_path.split("/")[-1]
+    filename = filetgz.replace('.tgz', '')
+
+    newdir = "/data/web_static/releases/" + filename
+
     try:
-        file = archive_path.split('/')[-1].split('.')[0]
-        # Upload the archive to the /tmp/ directory of the web server
         put(archive_path, "/tmp/")
-        # Create the folder to uncompress the archive
-        run('mkdir -p /data/web_static/releases/{}/'.format(file))
-        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}'
-            .format(file, file))
-        # Remove file from /tmp/ dir
-        run('rm /tmp/{}.tgz'.format(file))
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}'.format(file, file))
-        # Deletes existing symbilic link
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(file))
-        run('rm -rf /data/web_static/current')
-        # New symbolic link
-        run('ln -s /data/web_static/releases/{}\
-            /data/web_static/current'.format(file))
+        run("mkdir {}/".format(newdir))
+        run("tar -xzf /tmp/{} -C {}/".format(filetgz, newdir))
+        run("rm /tmp/{}".format(filetgz))
+        run("mv {}/web_static/* {}/".format(newdir, newdir))
+        run("rm -rf {}/web_static".format(newdir))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(newdir))
         return True
-    except Exception as e:
+    except:
         return False
