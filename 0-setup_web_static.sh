46a7e17c64ee110551c9d8ed9d8f0f56ebdbeef5
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-09-11 21:37:04 -0500

    install nginx

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,47 +1,18 @@
 #!/usr/bin/env bash
-
-if [ ! -e /var/run/nginx.pid ]; then
-    sudo apt-get -y update
-    sudo apt-get -y install nginx
-    sudo service nginx start
-fi
-
-if [[ ! -d /data/ ]]; then
-    sudo mkdir /data
-fi
-
-if [[ ! -d /data/web_static/ ]]; then
-    sudo mkdir /data/web_static
-fi
-
-if [[ ! -d /data/web_static/releases/ ]]; then
-    sudo mkdir /data/web_static/releases
-fi
-
-if [[ ! -d /data/web_static/shared/ ]]; then
-    sudo mkdir /data/web_static/shared
-fi
-
-if [[ ! -d /data/web_static/releases/test/ ]]; then
-    sudo mkdir /data/web_static/releases/test
-fi
-
-if [[ ! -f /data/web_static/releases/test/index.html ]]; then
-    sudo touch /data/web_static/releases/test/index.html
-    echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
+#Bash script that sets up your web servers for the deployment of web_static
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/ /data/web_static/shared/
+sudo touch -p /data/web_static/releases/test/index.html
+echo "<html>
+<head>
+</head>
+<body>
+Holberton School
+</body>
 </html>" >> /data/web_static/releases/test/index.html
-fi
-
-if [[ !-f /data/web_static/current ]]; then
-    sudo ln -s /data/web_static/releases/test/ data/web_static/current
-else
-    sudo rm data/web_static/current
-    sudo ln -s /data/web_static/releases/test/ data/web_static/current
-fi
-
-chown -R 755 /data/
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '48i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx restart
