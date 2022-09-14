067cacff7e6d7c8295e88e3b7cba32f8718badf2
TEOACEVEDO <teoacevedo.ma@gmail.com>
2022-09-13 19:13:41 -0500

    4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,39 +1,36 @@
 #!/usr/bin/python3
-"""script that starts a flask web application"""
-
+"""
+script that starts a Flask web application
+"""
 from flask import Flask
+from markupsafe import escape
+
 
 app = Flask(__name__)
 
-
-@app.route('/', strict_slashes=False)
-def web_hello():
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
     return "Hello HBNB!"
 
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return 'HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)
-def web_hbnb():
-    return "HBNB"
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def web_c(text):
-    space = text.replace('_', ' ')
-    return f"C {space}"
-
+@app.route("/c/<text>", strict_slashes=False)
+def C_is_(text):
+    C_space = text.replace('_', ' ')
+    return "C {}".format(C_space)
 
 @app.route("/python", strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def web_python(text="is cool"):
-    space_2 = text.replace('_', ' ')
-    return f"Python {space_2}"
-
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
 
 @app.route("/number/<int:n>", strict_slashes=False)
-def web_number(n):
+def number(n):
     if type(n) == int:
-        return f"{n} is a number"
-
+        return "{} is a number".format(n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
